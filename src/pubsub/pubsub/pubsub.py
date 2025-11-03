17309f82fdf6ad7f4e91e063e59d5472aa7e0c58
student <angushay42@gmail.com>
2025-11-03 11:12:28 +0000

    figured out some math stuff, but it doesn't continuously move. How does ros2 publish? what is d and t?

--- src/pubsub/pubsub/pubsub.py
+++ src/pubsub/pubsub/pubsub.py
@@ -3,6 +3,14 @@ from rclpy.node import Node
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist 
 import math
+import time
+
+MAX_VEL = 1.0
+"""
+R = v / w
+R is radius length? v is linear vel, w is angular vel
+R is length from ICC to centre of robot (P)
+"""
 
 """
 Adapt your controller for LIMO differential and mecanum mode by publishing to /cmd_vel accordingly.
@@ -15,30 +23,73 @@ class MinimalPublisher(Node):
     def __init__(self):
         super().__init__('minimal_publisher')
         self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
-        timer_period = 0.1  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        self.circle(0.5, 180, 0.2)
+        
+        # timer_period = 0.1  # seconds
+        # self.timer = self.create_timer(timer_period, self.timer_callback)
+        # self.i = 0
 
     def timer_callback(self):
         # msg = String()
         # msg.data = 'Hello World: %d' % self.i
         # self.publisher_.publish(msg)
 
+
         cmd_vel = Twist()
         cmd_vel.linear.x = 0.2  # m/s
         cmd_vel.angular.z = math.radians(90)
 
         self.publisher_.publish(cmd_vel)
-        self.get_logger().info('Publishing: "%s"' % cmd_vel.linear.x)
+        self.get_logger().info('Publishing: "%s"' % cmd_vel)
 
         self.i += 1
 
-    def circle(self, radius, start_dir):
-        # drive forward from origin to radius
-        # turn either either left or right (start_dir)
+    def circle(self, radius: float, start_dir: float, vel: float):
+        if not (0 <= start_dir < 360):
+            raise TypeError("invalid start direction")
         
+        # ensure velocity is < max_vel
+        if not (0 < vel <= MAX_VEL):
+            raise TypeError("invalid velocity")
+        
+        if radius == 0:
+            raise TypeError("invalid radius")
+        
+        start_dir = math.radians(start_dir)
 
-        pass
+        # turn start_dir
+        # drive forward from origin to radius
+        st = Twist()
+        st.linear.x = vel
+        st.angular.z = start_dir
+
+        # time = distance / speed
+        time_st = radius / vel
+        
+        self.publisher_.publish(st)
+        self.get_logger().info('Publishing: "%s"' % st)
+
+        # robot will move at vel speed and we will stop it 
+        # after t seconds, moving it d distance
+        time.sleep(time_st)
+
+        st.linear.x = 0.0
+        st.angular.z = 0.0
+
+        self.publisher_.publish(st)
+        self.get_logger().info('Publishing: "%s"' % st)
+
+        # calculate w as v/R
+        cir = Twist()
+        cir.linear.x = vel
+        cir.angular.z = math.radians(vel/radius)
+
+        # circumference = 2*pi*r, which is distance
+        self.publisher_.publish(cir)
+        self.get_logger().info('Publishing: "%s"' % cir)
+
+        time.sleep(2 * math.pi * radius)
+        
 
 def main(args=None):
     rclpy.init(args=args)
