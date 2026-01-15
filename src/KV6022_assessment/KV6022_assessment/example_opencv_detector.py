fedb141e1fde55af2afaafc5ec918a4f7881418e
angoose <angushay42@gmail.com>
2026-01-15 20:16:15 +0000

    Getting markers to display but accuracy is low, think I need to do it as a group (vectorise)

--- src/KV6022_assessment/KV6022_assessment/example_opencv_detector.py
+++ src/KV6022_assessment/KV6022_assessment/example_opencv_detector.py
@@ -16,12 +16,12 @@ from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import Header
 from cv_bridge import CvBridge, CvBridgeError
 from tf2_geometry_msgs import do_transform_pose
-from visualization_msgs.msg import Marker, MarkerArray
+from visualization_msgs.msg import Marker
 
 # student imported libraries (mine!)
 import numpy as np
 import random
-from geometry_msgs.msg import Pose, Point, Quaternion, TransformStamped
+from geometry_msgs.msg import Pose, Point, Quaternion, PoseArray
 
 
 
@@ -33,7 +33,8 @@ class ObjectDetector(Node):
     # global_frame = "base_link"  # unsure, is it world? map?
     global_frame = "map"    # it's map but navigation needs to be set up first
 
-    found_potholes = []
+    found_objs: list[Pose] = []     # should be hashmap
+    obj_thresh = 0.2                # how accurate is this?
 
     visualisation = True
     # aspect ration between color and depth cameras
@@ -49,7 +50,6 @@ class ObjectDetector(Node):
         self.depth_val = -1
 
         # subscribe to topic, when given data call callback
-        # todo question, what is data? 
         self.camera_info_sub = self.create_subscription(
             CameraInfo, 
             '/limo_camera/depth/camera_info',   # sensor_msgs/msg/CameraInfo
@@ -62,42 +62,47 @@ class ObjectDetector(Node):
             self.image_color_callback, 
             10
         )
-        self.image_sub = self.create_subscription(
+        self.image_depth_sub = self.create_subscription(
             Image, 
             '/limo_camera/depth/image_raw',     # sensor_msgs/msg/Image
             self.image_depth_callback, 
             10
         )
         
-        self.marker_array_pub = self.create_publisher(
-            MarkerArray, 
+        self.marker_pub = self.create_publisher(
+            Marker, 
             '/limo/object_markers', 
             10
         )
+
+        # ============================ Pothole ============================
         self.pothole_pub = self.create_publisher(
-            PoseStamped,
+            PoseArray,
             '/pothole',
+            10
+        )
+        self.pothole_sub = self.create_subscription(
+            PoseArray, 
+            '/pothole',
+            self.pothole_callback,
+            10
+        )
+
+        # ============================ end Pothole ============================
+
+
+        self.marker_sub = self.create_subscription(
+            Marker,
+            '/limo/object_markers',
+            self.marker_callback,
             10
         )
 
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
         print('finished init')
-
-    def get_tf_transform(self, target_frame, source_frame):
-        try:
-            transform = self.tf_buffer.lookup_transform(
-                target_frame, 
-                source_frame, 
-                rclpy.time.Time()
-            )
-            return transform
-        except Exception as e:
-            self.get_logger().warning(f"Failed to lookup transform: {str(e)}")
-            return None
     
     def camera_info_callback(self, data: CameraInfo):
-        print('camera info callback')
         if not self.camera_model:
             self.camera_model = image_geometry.PinholeCameraModel()
         self.camera_model.fromCameraInfo(data)
@@ -105,34 +110,20 @@ class ObjectDetector(Node):
         self.camera_frame = data.header.frame_id   # it's most likely depth_link
 
     def image_depth_callback(self, data: Image):
-        print('image depth callback')
         self.image_depth_ros = data
 
-
     def image_color_callback(self, data: Image):
-        print('image colour callback')
-
         # wait for camera_model and depth image to arrive
-        if self.camera_model is None:
+        if not all([self.camera_frame, self.camera_model, self.image_depth_ros]):
             return
-
-        if self.image_depth_ros is None:
-            return
-        
-        if self.camera_frame is None:
-            return
+        self.get_logger().info('COLOUR CALLBACK')
 
         # covert images to open_cv
         try:
             image_color = self.bridge.imgmsg_to_cv2(data, "bgr8")
             image_depth = self.bridge.imgmsg_to_cv2(self.image_depth_ros, "32FC1")
         except CvBridgeError as e:
-            print(e)
-
-
-        # PROVIDE YOUR OBJECT DETECTION CODE HERE
-        cx, cy = self.camera_model.cx(), self.camera_model.cy()
-        cx, cy = self.camera_model.rectifyPoint((cx, cy))
+            self.get_logger().warn(f"Bridge error: {e}")
         
         hsv_img = cv2.cvtColor(image_color, cv2.COLOR_BGR2HSV)
 
@@ -149,12 +140,15 @@ class ObjectDetector(Node):
             cv2.CHAIN_APPROX_SIMPLE
         )
 
-        world_poses = []
+        poses = PoseArray()
+        poses.header.frame_id = self.global_frame
+        poses.header.stamp = rclpy.time.Time().to_msg()
 
         for cnt in contours:
+            self.get_logger().info('CHECKING CONTOURS')
             img_moments = cv2.moments(cnt)
             if img_moments["m00"] <= 0:
-                print("invalid contour {}".format(cnt))
+                self.get_logger().warn("invalid contour {}".format(cnt))
                 continue
             centre = (
                 int(img_moments["m10"] / img_moments["m00"]), 
@@ -168,7 +162,7 @@ class ObjectDetector(Node):
             if ((0 < centre[0] <= self.image_depth_ros.width) 
                 and (0 < centre[1] <= self.image_depth_ros.height)
             ):
-                depth = image_depth[centre[1], centre[0]]
+                depth = image_depth[centre[1], centre[0]]   # flipped
 
             # scale unit vector (ray) with depth (in metres)
             ray *= depth
@@ -194,33 +188,50 @@ class ObjectDetector(Node):
                         rclpy.time.Time(),
                     )
                 )
-                
-                world_poses.append(global_pose)
-                self.pothole_pub.publish(
-                    PoseStamped(
-                        header=Header(
-                            frame_id=self.global_frame,
-                            stamp=rclpy.time.Time().to_msg()
-                        ),
-                        pose=global_pose
-                    )
-                )
-            except Exception as e:
-                self.get_logger().warn("could not retrieve transform")
 
+                poses.poses.append(global_pose)
+            except Exception as e:
+                self.get_logger().warn(f"could not retrieve transform: {e}")
+
+        self.get_logger().info('Attempting publish...')
+        self.pothole_pub.publish(poses)
 
         cv2.imshow("colour image", image_color)
         cv2.imshow("detection mask", image_depth)
         cv2.waitKey(1)
-        
-        marker_array = MarkerArray()
-        
-        for num, p in enumerate(world_poses):
+
+    def check_pose_exists(self, pose: Pose):
+        """New object will return False. True if exists"""
+        self.get_logger().info(
+            "Objects:" + ("{}, "*len(self.found_objs)).format(
+                *[f"x: {x.position.x:2f}, y: {x.position.y:2f}" for x in self.found_objs]
+            )
+        )
+        for o in self.found_objs:
+            self.get_logger().info('Checking objects...')
+            dist = np.sqrt(
+                ((o.position.x - pose.position.x)**2)
+                + ((o.position.y - pose.position.y)**2)
+                + ((o.position.z - pose.position.z)**2)
+            )
+            if dist < self.obj_thresh:
+                return True
+        return False
+
+    def pothole_callback(self, data: PoseArray):
+        # filter poses based on some threshold
+        for num, pose in enumerate(data):
+            self.get_logger().info('checking pose:')
+            if self.check_pose_exists(pose):
+                return
+            self.get_logger().info('appending pose')
+            self.found_objs.append(pose)
+
             marker = Marker()
-            marker.header.frame_id = "map"
+            marker.header.frame_id = self.global_frame
             marker.type = Marker.SPHERE
             marker.action = Marker.ADD
-            marker.pose = p     # pose goes here
+            marker.pose = data.pose
             marker.scale.x = 0.1
             marker.scale.y = 0.1
             marker.scale.z = 0.1
@@ -229,10 +240,12 @@ class ObjectDetector(Node):
             marker.color.g = 0.0
             marker.color.b = 0.0
             marker.id = num
-
-            marker_array.markers.append(marker)
         
-        self.marker_array_pub.publish(marker_array)
+        self.marker_pub.publish(marker)
+
+    def marker_callback(self, data: Marker):
+        self.get_logger().warn('MARKER: {}'.format(data.id))
+
 
 def main(args=None):
     print('Starting OpenCV detector node.')
